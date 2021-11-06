c6e2202d263dce4ec883b732e3623ab0e5a81c67
wisnu-aji <wisnupriambodo465@gmail.com>
2021-11-06 10:23:49 +0700

    tes mengganti endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require("express")
-const port = process.env.PORT || 3000
+const express = require("express");
+const port = process.env.PORT || 80;
 
-const app = express()
-app.get("/", (req, res) => {
-  res.json({ status: "OK" })
-})
+const app = express();
+app.get("/status/:nama", (req, res) => {
+  const id = req.params.nama;
+  console.log(id);
+  res.json({ status: "OK" });
+});
 
-app.listen(port)
+app.listen(port);
