68e030c63ff9fe4ce1b1b84ee599f2655b759123
awx <callmeoniichandesu@gmail.com>
2021-11-06 10:03:22 +0700

    Fix: syntac error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const port = process.env.PORT || 3000
 
 const app = express()
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "OK" })
 })
 
