f2354fc2e6b024f9233f2a71663d33afd9920596
wisnu-aji <wisnupriambodo465@gmail.com>
2022-06-02 07:28:53 +0700

    membuat list dan stat endpoint

--- src/routes/api/v1/user.ts
+++ src/routes/api/v1/user.ts
@@ -31,6 +31,85 @@ router.post('/edit', async (req, res) => {
   }
 })
 
+interface List {
+  page: number
+  limit: number
+  sortBy: 'nama' | 'alamat' | 'telepon' | null
+  status: 'telat-bayar' | 'sudah-bayar' | null
+}
+
+router.post('/list', async (req, res) => {
+  try {
+    const body = req.body as List
+    const payload =
+      body.status === 'telat-bayar'
+        ? { batasPembayaran: { $gt: new Date() } }
+        : body.status === 'sudah-bayar'
+        ? { batasPembayaran: { $lte: new Date() } }
+        : {}
+    const sortPayload = body.sortBy ? { [body.sortBy]: 1 } : {}
+    const list = await Pelanggan.find(payload)
+      .skip((+body.page - 1) * +body.limit)
+      .limit(+body.limit)
+      .sort(sortPayload)
+    res.json(list)
+  } catch (error: any) {
+    res.status(500).json({ ok: false, message: error.message })
+  }
+})
+interface Query {
+  query: string
+  page: number
+  limit: number
+}
+
+router.post('/search', async (req, res) => {
+  try {
+    const body = req.body as Query
+    if (+body.query) {
+      const list = await Pelanggan.find({
+        $or: [{ telepon: +body.query }, { _id: +body.query }],
+      })
+        .skip((+body.page - 1) * +body.limit)
+        .limit(+body.limit)
+      res.json(list)
+    } else {
+      const query = new RegExp(body.query, 'i')
+      const list = await Pelanggan.find({
+        $or: [{ nama: query }, { alamat: query }],
+      })
+        .skip((+body.page - 1) * +body.limit)
+        .limit(+body.limit)
+
+      res.json(list)
+    }
+  } catch (error: any) {
+    res.status(500).json({ ok: false, message: error.message })
+  }
+})
+interface Stat {
+  status: 'telat-bayar' | 'sudah-bayar'
+}
+router.post('/stat', (req, res) => {
+  try {
+    const body = req.body as Stat
+    const list =
+      body.status === 'telat-bayar'
+        ? { batasPembayaran: { $gt: new Date() } }
+        : body.status === 'sudah-bayar'
+        ? { batasPembayaran: { $lte: new Date() } }
+        : {}
+    Pelanggan.countDocuments(list, (err, count) => {
+      if (err) {
+        res.status(500).json({ ok: false, message: err.message })
+        return
+      }
+      res.json({ count })
+    })
+  } catch (error: any) {
+    res.status(500).json({ ok: false, message: error.message })
+  }
+})
 router.post('/remove', async (req, res) => {
   try {
     const body = req.body as PelangganType
